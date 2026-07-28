937ab160bd3023d0375fcf03ef55636bf7e8089e
Amina Yakdhan <yakdamina29@gmail.com>
2026-07-28 01:26:05 +0100

    Add files via upload

---
+++ Product.js
@@ -0,0 +1,33 @@
+
+const products = [
+  {
+    id: 1,
+    name: "Hoodie",
+    price: "$39.99",
+    description: "Soft cotton hoodie for everyday comfort.",
+    image: "https://straight-outta-cotton.com/cdn/shop/files/oversize-hoodie-peach-latte-peach-latte-straight-outta-cotton-4053994.jpg?v=1751992835",
+  },
+  {
+    id: 2,
+    name: "Graphic T-Shirt",
+    price: "$24.99",
+    description: "100% cotton graphic T-shirt with a modern design.",
+    image: "https://images.stylishop.com/cdn-cgi/image/format=avif/media/catalog/product/7022694536/images/7022694536_3.jpg?v=1",
+  },
+  {
+    id: 3,
+    name: "Blue Denim Jacket",
+    price: "$69.99",
+    description: "Classic denim jacket perfect for all seasons.",
+    image: "https://www.urbanofashion.com/cdn/shop/files/jakt-denimball-lblue-1_093a5521-45b0-4a38-8692-819980df8bca.jpg?v=1759710345",
+  },
+  {
+    id: 4,
+    name: "Jeans",
+    price: "$49.99",
+    description: "Effortlessly cool and ultra-comfortable jeans for everyday wear.",
+    image: "https://ringoffire.com/cdn/shop/files/RYB1533LAIDBACK-BERMUDA-FRONT.jpg?v=1758915141&width=740",
+  },
+];
+
+export default products;
